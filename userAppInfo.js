b34e2f0681999a2ba76e506dec05739ff6aa40fc
Iuliia Glazeva <zhotter@mail.ru>
2017-05-22 11:46:23 +0200

    created search function

--- userAppInfo.js
+++ userAppInfo.js
@@ -1,150 +1,66 @@
-var app = require('express')();
 var fs = require('fs');
-const bodyParser = require('body-parser')
-
-app.use(bodyParser.urlencoded({
-	extended: false
-}))
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
 
 
-app.set('views', 'Views');
+
+app.set('views', 'views');
 app.set('view engine', 'pug');
 
-// Part 0
-// Create one route:
-// - route 1: renders a page that displays all your users.
+app.use(bodyParser.urlencoded (
+	{extended: true}));
 
-app.get('/', (request, response) => {
-	fs.readFile('./users.json', 'utf-8', function(err,data){
-		if(err){
-			throw err;
-		}
+app.get('/', (req, res) => {
+	res.render('usersInfo');
 
+});
 
-		var parseData = JSON.parse(data);
-		
-		response.render("usersInfo", {
-			users: parseData
-		});
-	});
+app.get('/searchUsers', (req, res) =>{
+	res.render('searchUsers');
 });
 
 
-// Part 1
-// Create two more routes:
-// - route 2: renders a page that displays a form which is your search bar.
-// - route 3: takes in the post request from your form, then displays matching users on a new page. Users should be matched based on whether either their first or last name contains the input string.
 
-app.get('/searchUsers', (request, response) => {
-	console.log("reached")
-	response.render('searchUsers')
-})
+app.post('/searchUsers', (req, res) => {
+	res.render('searchUsers');
+});
 
-app.post('/searchUsers', (request, response) => {
-	fs.readFile('./users.json', 'utf-8', function(err, data){
-		// if (err) {
-		// 	throw err;
-		// }
 
-		var data = JSON.parse(data);
+app.post('/autocomplete', (req, res) =>{
+		var input = req.body.input;
+		
+	fs.readFile('users.json', function(err, userdata){
+		userdata = JSON.parse(userdata);
+		// search function:
+		
+		userdata = userdata.map(function(user){
+			return{
+				firstname: user.firstname,
+				lastname: user.lastname
 
-		data = data.map(function(user){
-			return {
-			firstname: user.firstname,
-			lastname: user.lastname
-		};
+			};
 			
 		});
-		console.log(data);
+		console.log(userdata);
+
+		var results = [];
 		
-		response.send(data);
+		userdata.forEach(function(user){
+			if (user.firstname.startsWith(input) || user.lastname.startsWith(input)) {
+				results.push(user);
+			}
+
+		});
+
+		res.send(results);
 
 
-		
-		// var input = request.body.name;
-
-
-		// for (var i = 0; i < users.length; i++) {
-
-		// 	if (input === users[i].firstname) {
-
-		// 		var firstname = users[i].firstname;
-		// 		var lastname = users[i].lastname;
-		// 		var email = users[i].email;
-		// 	} else if(input === users[i].lastname){
-
-		// 		var firstname = users[i].firstname;
-		// 		var lastname = users[i].lastname;
-		// 		var email = users[i].email;
-
-		// 	} else if (input === users[i].firstname + " " + users[i].lastname) {
-
-		// 		var firstname = users[i].firstname;
-		// 		var lastname = users[i].lastname;
-		// 		var email = users[i].email;
-		// 	}
-
-		// };
-
-
-
-		// 	response.render('findUsers', {
-		// 	first: firstname,
-		// 	last: lastname,
-		// 	email: email
-		// });
-
-     });
-
- });
-
-
-// Part 2
-// Create two more routes:
-// - route 4: renders a page with three forms on it (first name, last name, and email) that allows you to add new users to the users.json file.
-
-
-app.get('/register', (request, response) => {
-	response.render('form');
+	})
+	
 });
 
- //- route 5: takes in the post request from the 'create user' form, then adds the user to the users.json file. Once that is complete, redirects to the route that displays all your users (from part 0).
+app.listen(3000, function(){
+	console.log('UserInfoApp listening on port 3000!')
 
-app.post('/register', (request, response) => {
-	console.log(request.body);
-
-	fs.readFile('./users.json', 'utf-8', function(err,data){
-		if(err){
-			throw err;
-		}
-
-		parseData = JSON.parse(data);
-		
-
-        
-		firstname = request.body.firstname;
-	    lastname = request.body.lastname;
-	    email = request.body.email;
-
-
-
-	    newUser = {
-	        firstname: request.body.firstname,
-	        lastname: request.body.lastname,
-	        email: request.body.email
-
-    };
-
-        parseData.push(newUser)
-
-        fs.writeFile('./users.json', JSON.stringify(parseData));
-        response.redirect('/')
-	  
-      });
-   });
-
-
-
-var listener = app.listen(3000, function(){
-	console.log('userInfoApp listening on port:'  + listener.address().port)
 });
